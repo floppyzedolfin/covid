57ca1db75a0d9aaa80b24905050225551a6db736
Pascal Bertrand <floppyzedolfin@gmail.com>
2021-05-06 19:59:57 +0200

    some uts

---
+++ server/api_test.go
@@ -0,0 +1,17 @@
+package server
+
+import "testing"
+
+func TestDepartements(t *testing.T) {
+	tt := map[string]struct {
+		data map[string][]entry
+	} {
+
+	}
+
+	for name, _ := range tt {
+		t.Run(name, func(t *testing.T) {
+
+		})
+	}
+}
